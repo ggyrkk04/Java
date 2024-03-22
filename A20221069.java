c8bb7c9d7a0063283c107d95bbac4e7d66035bbd
한유 <158028630+ggyrkk04@users.noreply.github.com>
2024-03-22 11:35:53 +0900

    Add files via upload

---
+++ A20221069.java
@@ -0,0 +1,33 @@
+
+/**
+
+ *프로그램 파일이름 또는 간단한 설명
+
+ *학과: 서비스디자인공학과
+
+ *학번:20221069
+
+ *이름:한유
+
+ *이메일: ggyrkk04@naver.com
+
+ *제출일:2024년3월--일
+
+ *제출번호:--주차--번
+
+ *출처:
+
+*/
+public class A20221069{
+    public static void main(String[] args){
+        int hour=11;
+        int minute=59;
+        System.out.print("Number of minutes since midnight: ");
+        System.out.println(hour*60+minute);
+        System.out.print("Fraction of the hour that has passed: ");
+        System.out.println(minute/60);
+        System.out.print("Percent of the hour that has passed: ");
+        System.out.println(minute*100/60);
+}
+
+}
